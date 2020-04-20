9f735d5c8ad35c3e306b4274f30081b18e7f6650
shakti-garg <shakti.garg@gmail.com>
2020-04-20 08:40:43 +0530

    add setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+
+from setuptools import setup, find_packages
+
+work_dir = os.path.dirname(os.path.realpath(__file__))
+
+setup(name='prometheus-kafka-metrics',
+      version='0.1',
+      description='Prometheus instrumentation library for confluent-kafka-python applications',
+      url='https://github.com/shakti-garg/prometheus_kafka_metrics.git',
+      author='Shakti Garg',
+      author_email='shakti.garg@gmail.com',
+      license='Apache License Version 2.0',
+      packages=find_packages(exclude=("tests", "tests.*")),
+      install_requires=['prometheus-client'],
+      zip_safe=False)
